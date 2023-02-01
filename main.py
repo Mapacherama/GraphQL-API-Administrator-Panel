e31f2d5599f520a060d00cbaaf445b822cd9fab0
JeromeTesselaar <jerome.tesselaar@hva.nl>
2023-02-01 09:13:50 +0100

    Feature: Cleaning up the code

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-import json
 
 import graphene
 from flask import Flask
 from flask_graphql import GraphQLView
 from graphene_file_upload.scalars import Upload
 
-import administrator.user_type
+import administrator.user_type as user_type
+import administrator.user_enums as user_enums
 
-user_module = administrator.user_type.UserType
+UserType = user_type.UserType
+GenderEnum = user_enums.GenderEnum
+LanguageEnum = user_enums.LanguageEnum
 
 load_dotenv()
 
@@ -26,71 +28,58 @@ conn = psycopg2.connect(
     password=DB_PASS
 )
 
+
 class UserInputType(graphene.InputObjectType):
-    class Meta:
-        fields = ('id', 'username', 'email', 'password')
+    email = graphene.String()
+    username = graphene.String()
+    password = graphene.String(required=True)
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(name=graphene.String(default_value="stranger"))
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
 
-    def resolve_hello(self, info, name, conn=conn):
+    def resolve_hello(self, info, name):
         return 'Hello ' + name
 
-class Query(graphene.ObjectType):
-    user = graphene.Field(user_module, id=graphene.Int(required=True))
-
     def resolve_user(self, info, id):
-        # Implement code to retrieve user data based on the id
-        user =  resolve_user(id)
-        return user
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id=%s", (id,))
+        user_data = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return UserType(
+            id=user_data[0],
+            username=user_data[1],
+            email=user_data[2],
+            password=user_data[3]
+        )
 
-def resolve_user(self, info, id):
-# Create a cursor for executing SQL commands
-    cursor = conn.cursor()
-
-    # Execute a SQL query to retrieve the user with the specified id
-    cursor.execute("SELECT * FROM users WHERE id=%s", (id,))
-
-    # Fetch the user data
-    user_data = cursor.fetchone()
-
-    # Close the cursor and the database connection
-    cursor.close()
-    conn.close()
-
-    # Return the user data
-    return user_module(
-        id=user_data[0],
-        username=user_data[1],
-        email=user_data[2],
-        password=user_data[3]
-    )            
-
-
-class Mutation(graphene.ObjectType):
-    upload_file = graphene.Field(Upload, file=Upload(required=True))
 
 class CreateUser(graphene.Mutation):
     class Arguments:
         input = UserInputType(required=True)
-    
-    user = graphene.Field(user_module, id = graphene.Int(required=True))
-    
+
+    user = graphene.Field(UserType)
+
     def mutate(self, info, input):
         user = UserType(
-            username=input['username'],
-            password=input['password'],
-            email=input['email']
+            username=input.username,
+            password=input.password,
+            email=input.email
         )
         user.save()
         return CreateUser(user=user)
 
 
+class Mutation(graphene.ObjectType):
+    upload_file = graphene.Field(Upload, file=Upload(required=True))
+    create_user = graphene.Field(CreateUser.Field())
+
+
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 app = Flask(__name__)
-
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view(
