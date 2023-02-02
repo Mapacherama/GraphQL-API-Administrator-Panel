c6f2e8537da7e1ef5941693872f267f1fb874be1
JeromeTesselaar <jerome.tesselaar@hva.nl>
2023-02-02 10:08:50 +0100

    CreateUser is implemented

--- backend/schema.py
+++ backend/schema.py
@@ -6,7 +6,7 @@ import account.schema
 class Query(account.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(account.schema.Mutation,  graphene.ObjectType):
+class Mutation(account.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
